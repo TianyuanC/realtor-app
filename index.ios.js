07d3b051a200be74ccfde9fa2edadab09b71cd90
Ryan Chu <tianyuan.chu@gmail.com>
2017-07-26 00:18:21 -0700

    adding search view and highlight marker

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {
     AppRegistry,
-    StyleSheet
+    StyleSheet,
+    View
 } from 'react-native';
+import { Container, Header, Item, Input, Icon, Button, Text, Badge } from 'native-base';
 import MapView from 'react-native-maps';
 
 export default class App extends Component {
@@ -14,32 +16,41 @@ export default class App extends Component {
     }
     render() {
         return (
-            <MapView
-                style={ styles.map }
-                initialRegion={{
-                    latitude: 49.246292,
-                    longitude: -123.116226,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+            <Container>
+                <Header searchBar rounded>
+                    <Item>
+                        <Icon name="ios-search" />
+                        <Input placeholder="Search" />
+                    </Item>
+                    <Button transparent>
+                        <Text>Search</Text>
+                    </Button>
+                </Header>
+                <MapView
+                    style={ styles.map }
+                    initialRegion={{
+                        latitude: 49.246292,
+                        longitude: -123.116226,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}>
+                    <MapView.Marker
+                        coordinate={{latitude: 49.24629,
+                            longitude: -123.116226}}>
+                        <Badge primary>
+                            <Text>$123</Text>
+                        </Badge>
+                    </MapView.Marker>
+                </MapView>
+            </Container>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    },
     map: {
         position: 'absolute',
-        top: 0,
+        top: 65,
         left: 0,
         right: 0,
         bottom: 0,
