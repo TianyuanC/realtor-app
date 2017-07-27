1a286dba6a1caa8eccef7e0dbc3f5cf6cc08a27c
Ryan Chu <tianyuan.chu@gmail.com>
2017-07-26 23:47:42 -0700

    using react navigation

--- index.ios.js
+++ index.ios.js
@@ -3,56 +3,11 @@ import {
     AppRegistry,
     StyleSheet
 } from 'react-native';
-import { Container, Badge, Text } from 'native-base';
-import MapView from 'react-native-maps';
-import SearchBar from './components/SearchBar'
+import MapScreen from './components/MapScreen'
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            region: {
-                latitude: 49.246292,
-                longitude: -123.116226,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }
-        }
-    }
-
-    componentDidMount() {
-        //api data fetch goes here
-    }
-
-    render() {
-        return (
-            <Container>
-                <SearchBar />
-                <MapView
-                    style={styles.map}
-                    region={this.state.region}
-                    >
-                    <MapView.Marker
-                        coordinate={{latitude: 49.24629,
-                            longitude: -123.116226}}>
-                        <Badge primary>
-                            <Text>$123</Text>
-                        </Badge>
-                    </MapView.Marker>
-                </MapView>
-            </Container>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    map: {
-        position: 'absolute',
-        top: 65,
-        left: 0,
-        right: 0,
-        bottom: 0,
-    }
+const App = StackNavigator({
+    MapScreen: { screen: MapScreen },
 });
 
 AppRegistry.registerComponent('realtor', () => App);
